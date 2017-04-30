4a4c5ef70075d31529059237f9314ee4ea486f47
Yaroslav Velianyk <yvelianyk@softjourn.com>
2017-04-30 23:03:15 +0300

    implemented upload image dialog

--- src/app/shared/components/modal-upload-form/modal-upload-form.component.ts
+++ src/app/shared/components/modal-upload-form/modal-upload-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-modal-upload-form',
@@ -8,9 +9,58 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalUploadFormComponent implements OnInit {
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(public activeModal: NgbActiveModal, private translateService: TranslateService) {}
+
+  fileForUploading: any;
+  previewImageUrl: string;
 
   ngOnInit() {
+    this._setUploadFilePlaceHolder();
+  }
+
+  onChange(event) {
+    this._updatePreviewImage(event);
+    this.fileForUploading = this._getFileFromInput(event);
+  }
+
+  isValid() {
+    return this.previewImageUrl;
+  }
+
+  // --------------------------------------------------------------------------------------------
+  // PRIVATE HELPERS:
+  // --------------------------------------------------------------------------------------------
+
+  private _setUploadFilePlaceHolder(): void {
+    this.fileForUploading = {};
+
+    switch (this.translateService.currentLang) {
+      case 'en': {
+        this.fileForUploading.name = 'Choose file...';
+        break;
+      }
+      case 'ru': {
+        this.fileForUploading.name = 'Выберите файл...';
+        break;
+      }
+    }
+  }
+
+  private _updatePreviewImage(event: any): void {
+    let files = event.srcElement.files;
+    let reader = new FileReader();
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(files[0]);
+  }
+
+  private _getFileFromInput(event: any): any {
+    let files = event.srcElement.files;
+    return files[0];
+  }
+
+  private _handleReaderLoaded(event: any): void {
+    let reader = event.target;
+    this.previewImageUrl = reader.result;
   }
 
 }
