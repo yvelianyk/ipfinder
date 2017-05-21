9896032c8bdafc15c08b5cb45838586ba7a7c477
Yaroslav Velianyk <yvelianyk@softjourn.com>
2017-05-21 17:16:54 +0300

    [CLIENT-AUTH] partially done login flow

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { FacebookService, LoginResponse } from 'ngx-facebook';
 import { Config } from './config.service';
 import { UserService } from './user.service';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class AuthService {
 
   isConnected: Promise<string> = null;
 
+  private authUrl = Config.serverApiUrl + '/auth';
+
   constructor (
     private fb: FacebookService,
-    private userService: UserService
+    private userService: UserService,
+    private http: Http
   ) {
     fb.init(Config.facebook);
 
@@ -19,12 +24,24 @@ export class AuthService {
     });
   }
 
-  login() {
+  // TODO: correct handle errors
+  fbLogin() {
     return this.fb.login()
       .then((response: LoginResponse) => {
         localStorage.setItem('token', response.authResponse.accessToken);
       })
       .catch((error: any) => console.error(error));
+  }
+
+  login(params): Promise<any> {
+    return this.http.post(this.authUrl, params)
+      .toPromise()
+      .then(response => {
+        localStorage.setItem('token', response.json().token);
+        return response.json();
+      }).catch( () => {
+        return {error: 'Invalid credentials'}
+      })
   }
 
   isLoggedIn(): Promise<boolean> {
--- src/app/shared/components/modal-signin-form/modal-signin-form.component.ts
+++ src/app/shared/components/modal-signin-form/modal-signin-form.component.ts
@@ -5,6 +5,8 @@ import { TranslateService } from '@ngx-translate/core';
 
 import { User } from '../../../core/models/user';
 
+// TODO: add validation when invalid credentials, make refactoring(interface for auth Responses)
+
 @Component({
   selector: 'app-modal-signin-form',
   templateUrl: './modal-signin-form.component.html',
@@ -13,7 +15,6 @@ import { User } from '../../../core/models/user';
 export class ModalSigninFormComponent {
   status: Promise<string> = null;
   submitted = false;
-  rememberMe = true;
   user = new User('', '');
 
   constructor (
@@ -25,11 +26,22 @@ export class ModalSigninFormComponent {
   }
 
   onSubmit() {
-    this.submitted = true;
+    this.auth.login(this.user).then((res) => {
+      if (res.error) {
+        this.user = new User('', '');
+        return;
+      }
+      this.submitted = true;
+      this.activeModal.dismiss();
+      window.location.reload();
+    });
   }
 
   submitFacebook() {
-    this.auth.login();
+    this.auth.fbLogin().then((res) => {
+      this.activeModal.dismiss();
+      window.location.reload();
+    });
   }
 
   private _initCurrentLanguage() {
